d76c1c56f5b9deaa79bc8e009630bf9a9567c43f
zhaojer <zhaojer@umich.edu>
2024-12-19 19:33:48 -0500

    Implement transition

---
+++ src/sections/Footer/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+function Footer() {
+  return <Container />;
+}
+
+export default Footer;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 200vh;
+
+  background: linear-gradient(to bottom, black 0%, white 100%);
+`;
